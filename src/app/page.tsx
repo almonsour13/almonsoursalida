c7bf489a47ef3b45d5fb638496c43d5eed50da10
almonsour13 <almonsoursalida@gmail.com>
2026-06-18 03:34:07 +0800

    refator: remove unused hooks declartion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@
 import Footer from "@/components/layout/footer";
 import Services from "@/components/section/services";
 import Skills from "@/components/section/skills";
-import { useState } from "react";
 import Contact from "../components/section/contact";
 import Hero from "../components/section/hero";
 import Projects from "../components/section/projects";
 
 export default function Home() {
-    const [isExpanded, setIsExpanded] = useState(false);
     return (
         <div className="flex flex-col gap-16 w-full relative ">
             <Hero />
